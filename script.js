ac3606f8c60864a3a9ea29c7ef0527bc947f2e4b
Philip Osarodion <Philiposarodion@gmail.com>
2024-04-13 23:53:35 +0100

    I created a count down clock where by a user can enter an event date and name and click submit to start the countdown. the user can also reset the count down.

--- script.js
+++ script.js
@@ -1,175 +1,119 @@
-const menu = [
-  {
-    id: 1,
-    title: "Egusi Soup",
-    category: "African",
-    price: 4500,
-    img: "./menu-images/Egusi-Soup-IG-1.jpg",
-    desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
-  },
-  {
-    id: 2,
-    title: "Fried rice",
-    category: "Intercontinental",
-    price: 4999,
-    img: "./menu-images/fried-rice.jpg",
-    desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
-  },
-  {
-    id: 3,
-    title: "godzilla milkshake",
-    category: "shakes",
-    price: 1800,
-    img: "./menu-images/item-3.jpeg",
-    desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
-  },
-  {
-    id: 4,
-    title: "Efo-Riro",
-    category: "African",
-    price: 5900,
-    img: "./menu-images/Efo-riro.jpeg",
-    desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `,
-  },
-  {
-    id: 5,
-    title: "Jollof rice",
-    category: "Intercontinental",
-    price: 2999,
-    img: "./menu-images/jollof-rice.jpg",
-    desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `,
-  },
-  {
-    id: 6,
-    title: "oreo dream",
-    category: "shakes",
-    price: 2590,
-    img: "./menu-images/item-6.jpeg",
-    desc: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`,
-  },
-  {
-    id: 7,
-    title: "Banga Soup",
-    category: "African",
-    price: 4999,
-    img: "./menu-images/Banga-Soup.jpg",
-    desc: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `,
-  },
-  {
-    id: 8,
-    title: "Jollof Spaghetti",
-    category: "Intercontinental",
-    price: 2990,
-    img: "./menu-images/jollof-spaghetti.jpg",
-    desc: `on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut  `,
-  },
-  {
-    id: 9,
-    title: "quarantine buddy",
-    category: "shakes",
-    price: 1290,
-    img: "./menu-images/item-9.jpeg",
-    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
-  },
-  {
-    id: 10,
-    title: "bison steak",
-    category: "Intercontinental",
-    price: 8990,
-    img: "./menu-images/item-10.jpeg",
-    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
-  },
-  {
-    id: 10,
-    title: "Fries & Burger",
-    category: "Italian",
-    price: 4999,
-    img: "./menu-images/item-2.jpeg",
-    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
-  },
-  {
-    id: 10,
-    title: "Pancake",
-    category: "Italian",
-    price: 6700,
-    img: "./menu-images/item-1.jpeg",
-    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
-  },
-  {
-    id: 10,
-    title: "Beef Burger",
-    category: "Italian",
-    price: 2800,
-    img: "./menu-images/item-5.jpeg",
-    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
-  },
-];
+const form = document.querySelector("form");
+let formContainer = document.getElementById("container-form");
 
-const sectionCenter = document.querySelector(".section-center");
+let countdownContainer = document.getElementById("countdown-container");
 
-const btnContainer = document.querySelector(".btn-container");
+const resetBtn = document.getElementById("resteBtn");
 
-// let arrayBtn = [...filterButtons];
-// arrayBtn = arrayBtn.join("");
-// console.log(arrayBtn);
+let countdownInterval;
+let dueDate;
 
-window.addEventListener("DOMContentLoaded", () => {
-  showMenuInUi(menu);
+// now create the event on the submit form
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const eventName = document.getElementById("name").value;
+  const eventDate = document.getElementById("date").value;
+  if (!eventName || !eventDate) {
+    alert("please eneter an event name and date");
+    return;
+  }
+  dueDate = new Date(eventDate).getTime();
 
-  const categories = menu.reduce(
-    function (initial, accumulator) {
-      if (!initial.includes(accumulator.category)) {
-        initial.push(accumulator.category);
-      }
-      return initial;
-    },
-    ["all"]
-  );
-  const categoryBtns = categories
-    .map((category) => {
-      return `<button type="button" class="filter-btn" data-clickCategory=${category}>
-        ${category}
-        </button>`;
-    })
-    .join("");
-  btnContainer.innerHTML = categoryBtns;
-  const filterButtons = document.querySelectorAll(".filter-btn");
-  filterForBtn(filterButtons);
+  startCountdown(eventName);
 });
 
-function filterForBtn(filterBtn) {
-  filterBtn.forEach((btn) => {
-    btn.addEventListener("click", (e) => {
-      const category = e.currentTarget.dataset.clickcategory;
-      const menuCategory = menu.filter((menuItems) => {
-        if (menuItems.category === category) {
-          return menuItems;
-        }
-      });
-      if (category === "all") {
-        showMenuInUi(menu);
-      } else {
-        showMenuInUi(menuCategory);
-      }
-    });
-  });
+function startCountdown(eventName) {
+  clearInterval(countdownInterval);
+  countdownInterval = setInterval(calcCountdown, 1000, eventName);
+  // resetButton(eventName);
 }
 
-const showMenuInUi = function (menuItems) {
-  let displayMenu = menuItems.map((item) => {
-    return `<article class="menu-item">
-          <img src="${item.img}" alt="menu item" class="photo" />
-          <div class="item-info">
-            <header>
-              <h4>${item.title}</h4>
-              <h4 class="price">${item.price}</h4>
-            </header>
-            <p class="item-text">
-              ${item.desc}
-            </p>
-          </div>
-        </article>`;
-  });
-  displayMenu = displayMenu.join("");
-  sectionCenter.innerHTML = displayMenu;
-};
+function calcCountdown(title) {
+  const currentTime = new Date().getTime();
+  const distance = dueDate - currentTime;
+
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  formContainer.classList.add("form-container");
+
+  countdownContainer.style.display = "block";
+
+  countdownContainer.innerHTML = `<h2>${title}</h2>
+  <div class="countdown">
+    <div class="day down">
+      <div class="day input">${days}</div>
+      DAYS
+    </div>
+    <div class="hour down">
+      <div class="hour input">${hours}</div>
+      HOURS
+    </div>
+    <div class="minutes down">
+      <div class="minutes input">${minutes}</div>
+      MINUTES
+    </div>
+    <div class="seconds down">
+      <div class="seconds input">${seconds}</div>
+      SECONDS
+    </div>
+  </div>`;
+  const resetBtn = document.createElement("button");
+  resetBtn.textContent = "Reset Countdown";
+  resetBtn.onclick = resetButton(title);
+  countdownContainer.append(resetBtn);
+  console.log("this button is being clicked");
+}
+
+// resetBtn.addEventListener("click", function () {
+//   clearInterval(countdownInterval);
+//   countdownContainer.innerHTML = `<h2></h2>
+// <div class="countdown">
+//   <div class="day down">
+//     <div class="day input">00</div>
+//     DAYS
+//   </div>
+//   <div class="hour down">
+//     <div class="hour input">00</div>
+//     HOURS
+//   </div>
+//   <div class="minutes down">
+//     <div class="minutes input">00</div>
+//     MINUTES
+//   </div>
+//   <div class="seconds down">
+//     <div class="seconds input">00</div>
+//     SECONDS
+//   </div>
+// </div>
+// <button id="resetBtn">Reset</button>`;
+
+// });
+function resetButton() {
+  clearInterval(countdownInterval);
+  countdownContainer.style.display = "none";
+  formContainer.classList.remove("form-container");
+  // countdownContainer.innerHTML = `<h2>${title}</h2>
+  // <div class="countdown">
+  //   <div class="day down">
+  //     <div class="day input">00</div>
+  //     DAYS
+  //   </div>
+  //   <div class="hour down">
+  //     <div class="hour input">00</div>
+  //     HOURS
+  //   </div>
+  //   <div class="minutes down">
+  //     <div class="minutes input">00</div>
+  //     MINUTES
+  //   </div>
+  //   <div class="seconds down">
+  //     <div class="seconds input">00</div>
+  //     SECONDS
+  //   </div>
+  // </div>`;
+}
